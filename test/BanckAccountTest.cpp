88aef72c5e68ae96c433101a05ee509f1224cfd1
Francesco Mazzola <francesco.mazzola00@gmail.com>
2023-03-17 17:58:40 +0100

    Added 2 tests: makeTransaction and  decrementBalance

--- test/BanckAccountTest.cpp
+++ test/BanckAccountTest.cpp
@@ -6,10 +6,28 @@
 #include "gtest/gtest.h"
 #include <IOSTREAM>
 
-TEST(BankAccount, incrementBalance){
+TEST(BankAccount, incrementBalance){ //test for testing the incrementBalance method
     BankAccount *b;
     b=new BankAccount();
     b->incrementBalance(105.8);
     EXPECT_FLOAT_EQ(b->getBalance(), 105.8); //for float comparison use EXPECT_FLOAT_EQ because EXPECT_EQ dosent work
+}
+TEST(BankAccount, decrementBalance){ //test for testing the decrementBalance method
+    BankAccount *b;
+    b=new BankAccount();
+    b->incrementBalance(105.8);
+    b->decrementBalance(105.8);
+    EXPECT_FLOAT_EQ(b->getBalance(), 0);
+}
+
+TEST(BankAccount, makeTransaction){ //test for testing the transaction method
+    BankAccount *b1;
+    BankAccount *b2;
+    b1=new BankAccount();
+    b2=new BankAccount();
+    b1->incrementBalance(105.8);
+    b1->makeTransaction(b2, 105.8);
+    EXPECT_FLOAT_EQ(b1->getBalance(), 0);
+    EXPECT_FLOAT_EQ(b2->getBalance(), 105.8);
 }
 
